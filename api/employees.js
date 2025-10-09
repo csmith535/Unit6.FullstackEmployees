6f505193962ae4bfc88bb4b75f1851b4be2b5778
Cameron Smith <cameron.smith3030@gmail.com>
2025-10-08 22:08:32 -0400

    routes started

--- api/employees.js
+++ api/employees.js
@@ -3,3 +3,22 @@ const router = express.Router();
 export default router;
 
 // TODO: this file!
+import {
+  getEmployees,
+  getEmployee,
+  updateEmployee,
+  deleteEmployee,
+} from "#db/queries/employees";
+
+router.route("/").get(async (req, res) => {
+  res.send("Welcome to the Fullstack Employees API");
+});
+
+router.route("/employees").get(async (req, res) => {
+  try {
+    const response = await getEmployees();
+    res.send(response);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
