73c188c719f0de383cb6a83260f8482053d5478e
Cameron Smith <cameron.smith3030@gmail.com>
2025-10-08 22:15:40 -0400

    post employees route

--- api/employees.js
+++ api/employees.js
@@ -8,17 +8,44 @@ import {
   getEmployee,
   updateEmployee,
   deleteEmployee,
+  createEmployee,
 } from "#db/queries/employees";
 
 router.route("/").get(async (req, res) => {
   res.send("Welcome to the Fullstack Employees API");
 });
 
-router.route("/employees").get(async (req, res) => {
-  try {
-    const response = await getEmployees();
-    res.send(response);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+router
+  .route("/employees")
+  .get(async (req, res) => {
+    try {
+      const response = await getEmployees();
+      res.send(response);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  })
+  .post(async (req, res) => {
+    if (!req.body) {
+      res.status(400).send({ error: "Body is required" });
+    }
+
+    const { name, birthday, salary } = req.body;
+
+    if (!name) {
+      return res.status(400).send({ error: "Name is required" });
+    }
+    if (!birthday) {
+      return res.status(400).send({ error: "Birthday is required" });
+    }
+    if (!salary) {
+      return res.status(400).send({ error: "Salary is required" });
+    }
+
+    try {
+      const response = await createEmployee({ name, birthday, salary });
+      res.status(201).send(response);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
